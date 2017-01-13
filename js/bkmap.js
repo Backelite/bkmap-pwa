ffe03be124a7a90d432a2c9ca7dcfaefe00f24b5
Laurent Janet <laurent.janet@backelite.com>
2017-01-13 23:25:32 +0100

    uses creativityRoom instead of creativeRoom

--- js/bkmap.js
+++ js/bkmap.js
@@ -1,6 +1,6 @@
 ( function() {
 		var rooms = {
-			  'creativeRoom': [
+			  'creativityRoom': [
 			    'B9407F30-F5F8-466E-AFF9-25556B57FE6D-60402-57974',
           'B9407F30-F5F8-466E-AFF9-25556B57FE6D-6231-29789'
         ],
